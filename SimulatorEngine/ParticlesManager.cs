bc1cc67b0bcb039fd5ea97e91e0e45a4213636dd
matfij <mateusz.michal.fijak@gmail.com>
2025-02-22 19:30:50 +0100

    Implement particles rendering

---
+++ SimulatorEngine/ParticlesManager.cs
@@ -0,0 +1,21 @@
+﻿namespace SimulatorEngine;
+
+public class ParticlesManager
+{
+    private HashSet<Particle> Particles = [];
+
+
+    public IEnumerable<Particle> GetParticles => Particles;
+
+
+    public ParticlesManager()
+    {
+        var gen = new Random();
+
+        for (int i = 0; i < 100; i++)
+        {
+            Particles.Add(new Particle(gen.Next(0, 1200), gen.Next(0, 600)));
+        }
+    }
+}
+
--- SimulatorUI/MainPage.xaml.cs
+++ SimulatorUI/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using SkiaSharp;
+﻿using SimulatorEngine;
+using SkiaSharp;
 using SkiaSharp.Views.Maui;
 
 namespace SimulatorUI;
@@ -6,41 +7,53 @@ namespace SimulatorUI;
 public partial class MainPage : ContentPage
 {
     private static readonly (int Width, int Height) CanvasSize = (1200, 600);
-    private readonly SKPaint CursorPaint = new()
+    private static readonly SKPaint CursorPaint = new()
     {
         StrokeWidth = 2,
         IsAntialias = true,
         Color = SKColors.GhostWhite,
         Style = SKPaintStyle.Stroke,
     };
-    private readonly SKBitmap ParticlesBitmap = new(CanvasSize.Width, CanvasSize.Height);
+
+    private readonly ParticlesManager ParticlesManager;
     private readonly System.Timers.Timer PaintTimer = new(20); // 1000ms / 20 = 50fps
-    private (float X, float Y) CanvasScale = (1, 1);
+    private readonly SKBitmap ParticlesBitmap = new(CanvasSize.Width, CanvasSize.Height);
+
     private (float X, float Y, float R) Cursor = (0, 0, 10);
 
     public MainPage()
     {
         InitializeComponent();
+        ParticlesManager = new ParticlesManager();
         PaintTimer.Elapsed += (sender, args) => MainThread.BeginInvokeOnMainThread(InvalidateCanvas);
         PaintTimer.Start();
     }
 
-    private void UpdateBitmap()
+    private unsafe void UpdateBitmap()
     {
         ParticlesBitmap.Erase(SKColors.Black);
-        //var pixels = (uint*)ParticlesBitmap.GetPixels();
+        var pixels = (uint*)ParticlesBitmap.GetPixels();
+        var maxIndex = CanvasSize.Width * CanvasSize.Height;
+
+        foreach (var particle in ParticlesManager.GetParticles)
+        {
+            int index = particle.X + particle.Y * CanvasSize.Width;
+            if (index >= 0 && index < maxIndex)
+            {
+                pixels[index] = Particle.Color;
+            }
+        }
     }
 
     private void OnPaintSurface(object sender, SKPaintSurfaceEventArgs args)
     {
-        CanvasScale = (args.Info.Width / CanvasSize.Width, args.Info.Height / CanvasSize.Height);
         UpdateBitmap();
 
         var canvas = args.Surface.Canvas;
         canvas.Clear(SKColors.Black);
-        canvas.Scale((CanvasScale.X + CanvasScale.Y) / 2);
 
-        canvas.DrawBitmap(ParticlesBitmap, 0, 0);
+        var targetRect = new SKRect(0, 0, args.Info.Width, args.Info.Height);
+        canvas.DrawBitmap(ParticlesBitmap, targetRect);
         canvas.DrawCircle(Cursor.X, Cursor.Y, Cursor.R, CursorPaint);
     }
 
@@ -48,18 +61,18 @@ public partial class MainPage : ContentPage
     {
         if (args.ActionType == SKTouchAction.Moved)
         {
-            Cursor.X = args.Location.X / CanvasScale.X;
-            Cursor.Y = args.Location.Y / CanvasScale.Y;
+            Cursor.X = args.Location.X;
+            Cursor.Y = args.Location.Y;
         }
         if (args.ActionType == SKTouchAction.WheelChanged)
         {
-            var radius = (int)(2 * (Cursor.R + args.WheelDelta / 10) / (CanvasScale.X + CanvasScale.Y));
+            var radius = (int)(Cursor.R + args.WheelDelta / 10);
             Cursor.R = Math.Clamp(radius, 1, 100);
         }
         if (args.ActionType == SKTouchAction.Pressed || args.ActionType == SKTouchAction.Moved)
         {
-            var centerX = (int)(args.Location.X / CanvasScale.X);
-            var centerY = (int)(args.Location.Y / CanvasScale.Y);
+            var centerX = (int)args.Location.X;
+            var centerY = (int)args.Location.Y;
         }
     }
 
