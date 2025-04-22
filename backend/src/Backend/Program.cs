ddb2aa17675651c5c4900209881939180d153a49
matfij <mateusz.michal.fijak@gmail.com>
2025-04-22 20:56:18 +0200

    Setup AWS CDK backend project

---
+++ backend/src/Backend/Program.cs
@@ -0,0 +1,14 @@
+﻿using Amazon.CDK;
+
+namespace Backend
+{
+    sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            new BackendStack(app, "BackendStack", new StackProps { });
+            app.Synth();
+        }
+    }
+}
