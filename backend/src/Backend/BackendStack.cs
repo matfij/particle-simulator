35696b31839ba59bcda24d349beda5cb0e216eba
Mateusz Fijak <37413788+matfij@users.noreply.github.com>
2025-04-23 20:55:51 +0200

    Setup AWS CDK backend project (#32)
    New Features
    Introduced a backend solution with AWS Lambda integration, including a function that processes string input and returns it in uppercase.
    Added infrastructure-as-code setup using AWS CDK for streamlined deployment and management.
    
    Documentation
    Provided comprehensive README with deployment instructions for the Lambda function using Visual Studio and command-line tools.
    
    Chores
    Added configuration files for project setup, deployment, and environment management.
    Included a .gitignore to exclude development and build artifacts from version control.

---
+++ backend/src/Backend/BackendStack.cs
@@ -0,0 +1,20 @@
+using Amazon.CDK;
+using Amazon.CDK.AWS.Lambda;
+using Constructs;
+
+namespace Backend
+{
+    public class BackendStack : Stack
+    {
+        internal BackendStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            var uploadLambdaProps = new FunctionProps()
+            {
+                Runtime = Runtime.DOTNET_8,
+                Handler = "UploadLambda::UploadLambda.Function::FunctionHandler",
+                Code = Code.FromAsset("./src/UploadLambda/bin/Release/net8.0/UploadLambda.zip"),
+            };
+            var uploadLambda = new Function(this, "UploadLambda", uploadLambdaProps);
+        }
+    }
+}
---
+++ backend/src/Backend/Program.cs
@@ -0,0 +1,14 @@
+﻿using Amazon.CDK;
+
+namespace Backend
+{
+    static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            new BackendStack(app, "BackendStack", new StackProps { });
+            app.Synth();
+        }
+    }
+}
