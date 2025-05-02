2752e09583c675f5ca80e9bdc7e6baa5a79e1bd5
matfij <mateusz.michal.fijak@gmail.com>
2025-05-02 12:14:25 +0200

    Add API Gateway

--- backend/src/UploadLambda/Function.cs
+++ backend/src/UploadLambda/Function.cs
@@ -3,15 +3,46 @@ using Amazon.DynamoDBv2.DataModel;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.RuntimeSupport;
 using Amazon.Lambda.Serialization.SystemTextJson;
+using Amazon.S3;
+using Amazon.S3.Model;
 using UploadLambda;
+
+var bucketName = "particle-simulation-bucket";
+var s3Client = new AmazonS3Client();
 
 var dbClient = new AmazonDynamoDBClient();
 var dbContext = new DynamoDBContext(dbClient);
 
-var handler = async (Simulation input, ILambdaContext context) =>
+var handler = async (FileUploadRequest input, ILambdaContext context) =>
 {
-    await dbContext.SaveAsync(input);
-    return input.Id;
+    var fileName = $"{input.Name.ToLower().Replace(" ", "-")}-${Guid.NewGuid()}.json";
+
+    var simulation = new Simulation()
+    {
+        Id = Guid.NewGuid().ToString(),
+        Name = input.Name,
+        FileName = fileName,
+        Downloads = 0,
+    };
+
+    await dbContext.SaveAsync(simulation);
+
+    var request = new GetPreSignedUrlRequest
+    {
+        BucketName = bucketName,
+        Key = fileName,
+        Verb = HttpVerb.PUT,
+        Expires = DateTime.UtcNow.AddMinutes(15),
+        ContentType = input.ContentType,
+    };
+
+    var url = s3Client.GetPreSignedURL(request);
+
+    return new FileUploadResponse
+    {
+        UploadUrl = url,
+        FileKey = fileName,
+    };
 };
 
 await LambdaBootstrapBuilder.Create(handler, new DefaultLambdaJsonSerializer())
---
+++ backend/src/UploadLambda/Models.cs
@@ -0,0 +1,25 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+
+namespace UploadLambda;
+
+public class FileUploadRequest
+{
+    public required string Name { get; init; }
+    public required string ContentType { get; init; }
+}
+
+public class FileUploadResponse
+{
+    public required string UploadUrl { get; init; }
+    public required string FileKey { get; init; }
+}
+
+[DynamoDBTable("Simulation")]
+public class Simulation
+{
+    [DynamoDBHashKey]
+    public required string Id { get; init; }
+    public required string Name { get; init; }
+    public required string FileName { get; init; }
+    public int Downloads { get; set; }
+}
