f30f1eff83971f521ded68029383ce2e44b2aafc
matfij <mateusz.michal.fijak@gmail.com>
2025-06-03 21:14:57 +0200

    Adjust color based on temperature

--- SimulatorEngine/Particles/Powders.cs
+++ SimulatorEngine/Particles/Powders.cs
@@ -4,7 +4,19 @@ public class SandParticle : Particle
 {
     public override float Density => 1600f;
 
-    public override uint Color => 0xF6D7B0;
+    public override uint Color // 0xF6D7B0
+    {
+        get
+        {
+            int baseRed = 0xF6;
+            int redShift = (int)(Temperature / 8);
+
+            int red = baseRed + redShift;
+            if (red > 255) red = 255;
+
+            return (uint)((red << 16) | (0xD7 << 8) | 0xB0);
+        }
+    }
 
     public override ParticleKind Kind => ParticleKind.Sand;
 
@@ -36,7 +48,19 @@ public class SaltParticle : Particle
 {
     public override float Density => 2100f;
 
-    public override uint Color => 0xFCF9F3;
+    public override uint Color // 0xFCF9F3
+    {
+        get
+        {
+            int baseRed = 0xFC;
+            int redShift = (int)(Temperature / 8);
+
+            int red = baseRed + redShift;
+            if (red > 255) red = 255;
+
+            return (uint)((red << 16) | (0xF9 << 8) | 0xF3);
+        }
+    }
 
     public override ParticleKind Kind => ParticleKind.Salt;
 
@@ -75,7 +99,19 @@ public class StoneParticle : Particle
 {
     public override float Density => 2500f;
 
-    public override uint Color => 0x787A79;
+    public override uint Color // 0x787A79
+    {
+        get
+        {
+            int baseRed = 0x78;
+            int redShift = (int)(Temperature / 6);
+
+            int red = baseRed + redShift;
+            if (red > 255) red = 255;
+
+            return (uint)((red << 16) | (0x7A << 8) | 0x79);
+        }
+    }
 
     public override ParticleKind Kind => ParticleKind.Stone;
 
--- SimulatorEngine/Particles/Solids.cs
+++ SimulatorEngine/Particles/Solids.cs
@@ -4,7 +4,19 @@ public class IronParticle : Particle
 {
     public override float Density => 7800f;
 
-    public override uint Color => 0xA19D94;
+    public override uint Color // 0xA19D94
+    {
+        get
+        {
+            int baseRed = 0xA1;
+            int redShift = (int)(Temperature / 6);
+
+            int red = baseRed + redShift;
+            if (red > 255) red = 255;
+
+            return (uint)((red << 16) | (0x9D << 8) | 0x94);
+        }
+    }
 
     public override ParticleKind Kind => ParticleKind.Iron;
 
