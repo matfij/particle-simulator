debb5c3ed10c0879fed81f03bdfe8c25c6385a92
Mateusz Fijak <37413788+matfij@users.noreply.github.com>
2025-06-25 20:50:45 +0200

    Introduce Simulation file share  (#62)
    The changes introduce integration with CommunityToolkit.Maui, implement file sharing and saving functionality using dependency injection, and update localization resources for file operations. Several Android-specific platform files are removed, and the project file is updated to streamline target frameworks and add the CommunityToolkit.Maui dependency. The file sharing logic is now fully implemented, including user notifications.

--- SimulatorUI/Sharing/File/FileShareManager.cs
+++ SimulatorUI/Sharing/File/FileShareManager.cs
@@ -1,17 +1,40 @@
-﻿using SimulatorUI.Api;
+﻿using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Storage;
+using System.Text;
+using SimulatorUI.Api;
+using SimulatorUI.Resources.Locales;
+using SimulatorEngine;
 
 namespace SimulatorUI.Sharing.File;
 
-public class FileShareManager : IShareManager
+public class FileShareManager(IParticlesManager particlesManager) : IShareManager
 {
+    private readonly string _format = ".sim";
+    private readonly IParticlesManager _particlesManager = particlesManager;
+
     public Task<Stream> LoadSimulation(string simulationId, CancellationToken token = default)
     {
         throw new NotImplementedException();
     }
 
-    public Task ShareSimulation(string simulationName, string simulationData, CancellationToken token = default)
+    public async Task ShareSimulation(string simulationName = "", string simulationData = "", CancellationToken token = default)
     {
-        throw new NotImplementedException();
+        var name = String.Format(AppStrings.SimulationName, DateTime.Now.ToString("yyyy-MM-dd")) + _format;
+        var data = SimulationSerializer.Serialize(_particlesManager.Particles);
+
+        using var stream = new MemoryStream(Encoding.UTF8.GetBytes(data));
+        var fileSaverResult = await FileSaver.Default.SaveAsync(name, stream, token);
+
+        if (fileSaverResult.IsSuccessful)
+        {
+            await Toast.Make(String.Format(AppStrings.FileSaveSuccess, fileSaverResult.FilePath))
+                .Show(token);
+        }
+        else
+        {
+            await Toast.Make(String.Format(AppStrings.FileSaveError, fileSaverResult.Exception.Message ?? AppStrings.UnknownError))
+                .Show(token);
+        }
     }
 
     public Task<IEnumerable<SimulationPreview>> LoadSimulationsPreview(CancellationToken token = default)
